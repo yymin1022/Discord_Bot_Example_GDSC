067b299f378bc7dd47a70f43ca22b9f3a56fd735
yymin1022 <yymin1022@gmail.com>
2023-05-23 19:03:02 +0900

    Update main.py : Added Simple Replying Logic

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ class BotClient(discord.Client):
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
+        if message.author != self.user:
+            await message.channel.send(message.content)
+
 
 intents = discord.Intents.default()
 intents.message_content = True
