ca857ca2852ce4874bd9e7a55762834ba11dce91
yymin1022 <yymin1022@gmail.com>
2023-05-23 18:37:18 +0900

    Update main.py : Added Discord Bot Base Code

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-def print_hi(name):
-    print(f"Hi, {name}")
+import discord
 
 
-if __name__ == "__main__":
-    print_hi("PyCharm")
-    
+class BotClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = BotClient(intents=intents)
+client.run("DISCORD_BOT_TOKEN")
