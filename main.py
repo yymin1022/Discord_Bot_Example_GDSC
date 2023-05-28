48fd9d2626343c75649bf9fc95f48e3ead45aa15
yymin1022 <yymin1022@gmail.com>
2023-05-28 16:52:34 +0900

    Update main.py : Reply Bard API Message

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import discord
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
+BARD_API_URL = os.getenv("BARD_API_URL")
 BARD_SESSION_ID = os.getenv("BARD_SESSION_ID")
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 
 def get_bard_message(message):
-    pass
+    api_data = {
+        "message": message,
+        "session_id": BARD_SESSION_ID
+    }
+    
+    api_request = requests.post(BARD_API_URL, json=api_data)
+    return api_request.json()["content"]
 
 
 class BotClient(discord.Client):
@@ -19,7 +27,7 @@ class BotClient(discord.Client):
         print(f'Message from {message.author}: {message.content}')
 
         if message.author != self.user:
-            await message.channel.send(message.content)
+            await message.channel.send(get_bard_message(message.content))
 
 
 intents = discord.Intents.default()
